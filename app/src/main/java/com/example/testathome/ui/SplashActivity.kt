e31a1a730ee0c7dbd14c87b6085d935eb949cf6e
rlaal <rlaalgus132@naver.com>
2021-03-26 03:02:31 +0900

    map

--- app/src/main/java/com/example/testathome/ui/SplashActivity.kt
+++ app/src/main/java/com/example/testathome/ui/SplashActivity.kt
@@ -22,15 +22,6 @@ import com.google.android.material.snackbar.Snackbar
 
 class SplashActivity : AppCompatActivity() {
 
-    val PERMISSION_REQUEST_CODE = 1001
-    var NEVER_ASK_AGAIN=false
-
-    private val permissions = arrayOf(
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,79 +31,12 @@ class SplashActivity : AppCompatActivity() {
             this, R.layout.activity_splash
         )
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            when {
-                checkAllPermissionsGranted(permissions) -> {
-                    // You can use the API that requires the permission.
-                    Handler(Looper.myLooper()!!).postDelayed({  //Handler 생성 중에 암시 적으로 Looper를 선택하면 작업이 자동으로 손실 (Handler가 새 작업을 예상하지 않고 종료되는 경우)
-                        // , 충돌 (Looper가 활성화되지 않은 스레드에서 처리기가 생성되는 경우) 또는 경쟁 조건이 발생하는 버그가 발생할 수 있습니다.
-                        startActivity(Intent(this, MainActivity::class.java))
-                        finish()
-                    }, 3000)
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
 
-                }
-//                shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
-//                    finish()
-//                }
 
-                else -> {
-//                    if (!NEVER_ASK_AGAIN){
-//                        onRequestPermissionsResult(PERMISSION_REQUEST_CODE,permissions, intArrayOf(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-//                            ,checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)))
-//                    }
-
-                    // You can directly ask for the permission.
-                    // The registered ActivityResultCallback gets the result of this request.
-                    Handler(Looper.myLooper()!!).postDelayed({
-                        ActivityCompat.requestPermissions(
-                            this,
-                            permissions,
-                            PERMISSION_REQUEST_CODE
-                        )
-                    }, 3000)
-
-                }
-            }
-        }
-
-    }
-
-    fun checkAllPermissionsGranted(permissions: Array<String>): Boolean {
-
-        var hasGranted = true
-        for (permission in permissions) {
-            hasGranted = (hasGranted && ContextCompat.checkSelfPermission(applicationContext, permission
-            ) == PackageManager.PERMISSION_GRANTED)
-        }
-        return hasGranted
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>, grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PERMISSION_REQUEST_CODE -> {
-                if ((grantResults.isEmpty() ||
-                            PackageManager.PERMISSION_DENIED in grantResults)
-                ) {
-                    //권한 여러개일 때 하나라도 거절된 권한이 있으면
-                    Toast.makeText(this, "서비스 사용을 위해 권한 동의가 필요합니다.", Toast.LENGTH_SHORT).show()
-                    finish()
-                    return
-                } else {
-                    //모든 권한 동의 된 경우
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
-                }
-            }
-            // Add other 'when' lines to check for other
-            // permissions this app might request.
-            else -> {
-                // Ignore all other requests.
-            }
-        }
     }
 
 
--- app/src/main/java/com/example/testathome/ui/map/MapsFragment.kt
+++ app/src/main/java/com/example/testathome/ui/map/MapsFragment.kt
@@ -1,20 +1,31 @@
 package com.example.testathome.ui.map
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.fragment.findNavController
 import com.example.testathome.R
 import com.example.testathome.databinding.FragmentMapsBinding
 import com.example.testathome.models.Item
 import com.example.testathome.ui.BaseFragment
+import com.example.testathome.ui.MainActivity
 import com.example.testathome.ui.savedlist.search.SearchFragment
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -34,6 +45,45 @@ class MapsFragment : BaseFragment(), GoogleMap.OnMyLocationClickListener,
     private lateinit var fuseLocationProvicerClient: FusedLocationProviderClient
     lateinit var mLocationManager: LocationManager
     private var mMap: GoogleMap? = null
+    lateinit var mapFragment: SupportMapFragment
+
+    val PERMISSION_REQUEST_CODE = 1001
+    var NEVER_ASK_AGAIN=false
+
+    private val permissions = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            when {
+                checkAllPermissionsGranted(permissions) -> {
+                    val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+                    mapFragment?.getMapAsync(callback)
+                }
+
+                else -> {
+//                    if (!NEVER_ASK_AGAIN){
+//                        onRequestPermissionsResult(PERMISSION_REQUEST_CODE,permissions, intArrayOf(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+//                            ,checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)))
+//                    }
+
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                        ActivityCompat.requestPermissions(
+                            requireActivity(),
+                            permissions,
+                            PERMISSION_REQUEST_CODE
+                        )
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,14 +91,17 @@ class MapsFragment : BaseFragment(), GoogleMap.OnMyLocationClickListener,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_maps, container, false)
+        mapFragment = (childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?)!!
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        if( checkAllPermissionsGranted(permissions)){
+            val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+            mapFragment?.getMapAsync(callback)
+        }
 
         binding.saveItemButton.setOnClickListener {
             val dialogFragment = SearchFragment(currentLocation)
@@ -66,7 +119,7 @@ class MapsFragment : BaseFragment(), GoogleMap.OnMyLocationClickListener,
     }
 
     private fun moveCamera(latLng: LatLng, zoom: Float) {
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
+        mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
     }
 
     private fun getCurrentLocation() {
@@ -75,6 +128,7 @@ class MapsFragment : BaseFragment(), GoogleMap.OnMyLocationClickListener,
             .addOnSuccessListener { location: Location? ->
                 if (location != null) {
                     currentLocation = location
+                    mapFragment?.getMapAsync {callback }
                     moveCamera(LatLng(currentLocation.latitude, currentLocation.longitude), 18F)
                 }
             }
@@ -103,6 +157,47 @@ class MapsFragment : BaseFragment(), GoogleMap.OnMyLocationClickListener,
         getCurrentLocation()
     }
 
+    fun checkAllPermissionsGranted(permissions: Array<String>): Boolean {
+
+        var hasGranted = true
+        for (permission in permissions) {
+            hasGranted = (hasGranted && ContextCompat.checkSelfPermission(requireContext().applicationContext, permission
+            ) == PackageManager.PERMISSION_GRANTED)
+        }
+        return hasGranted
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if ((grantResults.isEmpty() ||
+                            PackageManager.PERMISSION_DENIED in grantResults)
+                ) {
+                    //권한 여러개일 때 하나라도 거절된 권한이 있으면
+                    Toast.makeText(requireContext(), "서비스 사용을 위해 권한 동의가 필요합니다.", Toast.LENGTH_SHORT).show()
+                    return
+                } else {
+//                   refreshFragment(this,mapFragment.childFragmentManager)
+//                    mapFragment.getMapAsync(callback)
+
+                }
+            }
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+    fun refreshFragment(fragment: Fragment, fragmentManager: FragmentManager) {
+        var ft: FragmentTransaction = fragmentManager.beginTransaction()
+        ft.detach(fragment).attach(fragment).commit()
+    }
 
 }
 
